b2f40921660e394af1bfe0461cbc551d5c7dfa9e
Shubham Kasaudhan <shubhamkasaudhan512@gmail.com>
2018-03-20 02:02:20 +0530

    Implementing phone page it message for your changes. Lines starting

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import './main.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import React from 'react'
+import ReactDOM from 'react-dom'
+// import { BrowserRouter, Link, Route } from 'react-router-dom'
+import {createStore, applyMiddleware} from 'redux'
+import {composeWithDevTools} from 'redux-devtools-extension'
+import thunk from 'redux-thunk'
+import {syncHistoryWithStore} from 'react-router-redux'
+import {Router, Route, browserHistory} from 'react-router'
+import {Provider} from 'react-redux'
+import createHistory from 'history/createBrowserHistory'
+
+import reducers from 'reducers'
+import Layout from 'containers/layout'
+import Phones from 'containers/phones'
+
+const store = createStore(reducers, composeWithDevTools(
+    applyMiddleware(thunk)
+))
+
+const history = syncHistoryWithStore(browserHistory, store)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Router history={history}>
+            <Route component={Layout}>
+                <Route path='/' component={Phones} />
+            </Route>
+        </Router>
+    </Provider>,
+    document.getElementById('root')
+);
